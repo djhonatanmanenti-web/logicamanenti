a8bde97b416717ba1a95ba19a0dc9560f8dd343f
djhonatanmanenti-web <djhonatan_manenti@estudante.sesisenai.org.br>
2026-06-18 17:25:12 -0300

    programas de hj

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+function guilherme(){
+    // ler estagiarios
+    let estagiarios = Number(prompt("quantos estagiarios"))
+    // ler clt
+    let clt = Number(prompt("quantos clt"))
+    // ler pj
+    let pj = Number(prompt("quantos pj"))
+    // calcular total
+    let total = estagiarios + clt + pj
+    // mostrar resultado
+    let resultado = ("quantidade de funcionarios ideal")
+}
+
+
+
+
+
+
+function banheiro(){
+    alert("ja fez ate o exercicios 2?")
+}
+
+function pepequeno(){
+    let qtdpares = Number(prompt("quantos pares?"))
+    let precopar = Number(prompt("preço de cada par?"))
+
+    let total = qtdPares * precopar
+
+    alert("valor total R$ + total")
+}
+
+
+
+
+
+
+function peba(){
+    //ler numero de vitorias
+    let vitorias = Number(prompt("quantas vitorias?"))
+    //ler numeros de empates
+    let empates = Number(prompt("quantos empates"))
+    // calcular pontos
+    let pontos = vitorias*3 + empates
+    // mostrar os resultados
+    alert("O time teu tem " + pontos)
+}
